f42a35d073c3ba04883e075cf48e705d30055b85
aryanpatel0318 <99696854+aryanpatel0318@users.noreply.github.com>
2023-11-09 15:22:36 -0500

    Create shapes.js

---
+++ shapes.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+// background (82, 0, 214);
+//  angleMode(DEGREES); //- used to set the mode 
+// arc(150,50,100,100,0,270, CHORD); // chord arc //- arc of a circle
+//  arc(250,200,100,100,0,270, PIE); // pie arc //- fragment of a circle missing
+//  arc(350,350,100,100,0,270, OPEN); // open arc //- the circle is open
+
+//  strokeWeight(5); //- changes the weight of the outline
+//  fill(0, 214, 189); //- changes the color of the shape
+//  rect(130,90,260,120); //- creates a rectangle (x,y,width,height)
+//  ellipse(370,190,140,200); //- creates a oval
+
+//  strokeWeight(10);
+// stroke(214, 0, 4); //- changes the color of the outline
+//  point(130, 365); //- creates a point
+//  point(150,255);
+//  point(170,165);
+//  point(190,95);
+//  point(210,45);
+//  point(230,15);
+//  point (230,5);
+//  point(270,15);
+//  point(290,45);
+//  point(310,95);
+//  point(330,165);
+//  point(350,255);
+//  point(370,365);
+  
+//  strokeWeight(5);
+//  stroke(0,0,255);
+//  line(40,0,width,height); //- creates a line
+
+//  triangle(80,150,136,40,192,150); //- creates a triangle (x1,y1)(x2,y2)(x3,y3)
+
+// quad(38,31,486,20,369,363,30,276); //- creates a quadrilateral
+
+}
+//function draw(){
+ // ellipse(mouseX,mouseY,50,50);
+//}
